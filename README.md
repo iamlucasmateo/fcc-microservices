1ea4478fc2f070a4e39a1e7e650c3d1358666b30
iamlucasmateo <perrone.lucasmateo@gmail.com>
2023-03-19 13:27:31 +0100

    authentication flow

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # fcc-microservices
-A simple microservices with message queueing architecture
+A simple microservices with message queueing architecture: https://www.youtube.com/watch?v=hmkF77F9TLw
--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -3,15 +3,25 @@ from flask import Flask, request
 from flask_mysqldb import MySQL
 
 server = Flask(__name__)
-mysql = MySQL(server)
+
+environment = {
+    "MYSQL_HOST": "localhost",
+    "MYSQL_USER": "root",
+    "MYSQL_PASSWORD": "AlieN3242DucE_",
+    "MYSQL_DB": "fcc_micros",
+    "MYSQL_PORT": 3306,
+    "JWT_SECRET": "my_jwt_secret"
+}
 
 # config
-for var in ["HOST", "USER", "PASSWORD", "DB", "PORT"]:
-    name = f"MYSQL_{var}"
-    server.config[name] = os.environ.get(name)
+for key, value in environment.items():
+    os.environ[key] = str(value)
+    server.config[key] = value
+
+mysql = MySQL(server)
 
 @server.route("/login", methods=["POST"])
-def loging():
+def login():
     # the request object contains data for simple authorization
     auth = request.authorization
     if not auth:
@@ -19,16 +29,60 @@ def loging():
     # check DB for user and pass
     cur = mysql.connection.cursor()
     values = (auth.username,)
-    query = "SELECT email, password FROM user WHERE email=%s"
+    query = "SELECT email, password, isAdmin FROM user WHERE email=%s"
     res = cur.execute(query, values)
     if res > 0:
         user_row = cur.fetchone()
-        email = user_row[0]
-        password = user_row[1]
+        email, password, is_admin = user_row
+        is_admin = bool(is_admin)
 
         if auth.username != email or auth.password != password:
             return "invalid credentials", 401
         else:
-            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), is_admin)
+    
     return "invalid credentials", 401
-        
+
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+    if not encoded_jwt:
+        return "missing credentials", 401
+    auth_type, token = encoded_jwt.split(" ")
+    try:
+        decoded = jwt.decode(
+            token,
+            os.environ.get("JWT_SECRET"),
+            algorithms=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+    
+    return decoded, 200
+
+
+
+def createJWT(username: str, secret: str, isAdmin: bool):
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    expiration = now + datetime.timedelta(days=1)
+    userData = {
+        "username": username,
+        "exp": expiration,
+        "iat": now,
+        "admin": isAdmin
+    }
+    return jwt.encode(userData,
+        secret,
+        algorithm="HS256"
+    )
+
+def validateJWT():
+    pass
+
+if __name__ == "__main__":
+    # this works for Docker networks. Flask app will be listening to all IPs within the Docker
+    # container that hosts them. The IP asigned to the Docker container is subject to change, 
+    # and the same container can have different addresses in different networks that it is part of.
+    # so, by listening on all IP adresses of the Docker container, the app will always get the requests directed at it.
+    all_ips = "0.0.0.0"
+    server.run(host=all_ips, port=5000)
